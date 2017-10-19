0fd5023ccbbcd4d38db4a28325da3ea4ba9b3b68
Elis Vathi <elis_v90@hotmail.com>
2017-10-20 00:34:52 +0200

    initial

---
+++ Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Test")]
+    [Authorize]
+    public class TestController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public TestController ( UserManager<ApplicationUser> manager )
+            {
+            _userManager = manager;
+            }
+        [HttpGet]
+        public async Task<IActionResult> Get ()
+            {
+            var user = await _userManager.GetUserAsync(User);
+            return Ok(user.Email);
+            }
+    }
+}
